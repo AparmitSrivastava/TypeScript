8cfc5d18ce7ba3dc5807fc38f37d544de0e10b1f
AparmitSrivastava <aparmitsrivastava1@gmail.com>
2025-05-18 00:31:56 +0530

    index signature

---
+++ 28_TS/index.ts
@@ -0,0 +1,22 @@
+// An index signature allows you to define the shape of an object when the property names (keys) are not known in advance, but you do know the type of the keys and the type of the values.
+
+type MyType = {
+  [key: string]: string;
+};
+// This means:
+// "This object can have any number of properties with string keys, and the values must all be strings."
+
+type ErrorBag = {
+  [field: string]: string;
+};
+
+const errors: ErrorBag = {
+  email: "Invalid email",
+  username: "Too short",
+  password: "Must include a symbol"
+};
+// All keys must be strings ,All values must be strings
+
+const invalid: ErrorBag = {
+//   age: 30 //  Error: number is not assignable to string
+};
